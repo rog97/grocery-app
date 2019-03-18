77a68dc798b96639c37d972314b953716ea2f2c9
Roger Teran <roger.teran@wework.com>
2019-03-18 01:31:27 -0400

    touch recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,7 +4,9 @@ const RecipeCard = (props) => {
   return (
     <div className="item">
       <div className="content">
-        <div className="header">{props.recipe.label}</div>
+        <div className="header">{props.recipe.label}</div><br/>
+        <p>Calories: {parseInt(props.recipe.calories)}</p>
+        <p>Total Time: {props.recipe.totalTime === 0 ? 30 : props.recipe.totalTime} min</p>
       </div>
     </div>
   );
